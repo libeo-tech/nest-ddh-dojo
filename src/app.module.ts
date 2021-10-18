7a1029ba513d3cfc41a55cc5ba8d8d1d45f77776
Pierre Chabardes <pierre.chabardes@libeo.io>
2021-10-18 08:58:37 +0200

    feat(*): configure typeorm and gql modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: [__dirname + '/../**/*.orm-entity.{js,ts}'],
+    }),
+  ],
 })
 export class AppModule {}
