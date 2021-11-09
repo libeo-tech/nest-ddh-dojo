4f41bc44e792a45c9f59f1e67a8eb55cb9cd51d8
Pierre Chabardes <pierre.chabardes@libeo.io>
2021-11-09 18:08:34 +0100

    draft(hero): use Hero saga as cron with the power of rxjs

--- src/heroes/core/application/sagas/hero.saga.ts
+++ src/heroes/core/application/sagas/hero.saga.ts
@@ -1,7 +1,18 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ICommand, ofType, Saga } from '@nestjs/cqrs';
-import { filter, from, map, mergeMap, Observable } from 'rxjs';
-import { GetByIdPort } from '../../../../common/core/ports/base.ports';
+import {
+  filter,
+  from,
+  interval,
+  map,
+  mergeAll,
+  mergeMap,
+  Observable,
+} from 'rxjs';
+import {
+  GetAllPort,
+  GetByIdPort,
+} from '../../../../common/core/ports/base.ports';
 import { DragonSlainEvent } from '../../../../dragons/core/domain/dragon.events';
 import { Hero } from '../../domain/hero.entity';
 import { HeroGainedXpEvent } from '../../domain/hero.events';
@@ -9,9 +20,32 @@ import { doesHeroLevelUp } from '../../domain/xp/xp.service';
 import { GainXpCommand } from '../commands/gain-xp/gain-xp.command';
 import { LevelUpCommand } from '../commands/level-up/level-up.command';
 
+const isHeroAlive = (hero: Hero): boolean => !!hero.id;
+
 @Injectable()
 export class HeroSagas {
-  constructor(@Inject(Hero) private readonly heroPorts: GetByIdPort<Hero>) {}
+  constructor(
+    @Inject(Hero)
+    private readonly heroPorts: GetByIdPort<Hero> & GetAllPort<Hero>,
+  ) {}
+
+  private everyMinuteForHeroesAliveTimer = interval(1000 * 60).pipe(
+    mergeMap(() => from(this.heroPorts.getAll()).pipe(mergeAll())),
+    filter(isHeroAlive),
+  );
+
+  @Saga()
+  healHero = (): Observable<ICommand> => {
+    return this.everyMinuteForHeroesAliveTimer.pipe(
+      map(
+        ({ id: heroId }) =>
+          new GainXpCommand({
+            heroId,
+            xpDelta: 0,
+          }),
+      ),
+    );
+  };
 
   @Saga()
   dragonSlain = (events$: Observable<any>): Observable<ICommand> => {
