58d443f37718f422ec6e51b417cc2894f7e96067
Pierre Chabardes <pierre.chabardes@libeo.io>
2022-05-11 08:26:46 +0200

    feature(equipItem): add expected errors in handler

--- src/heroes/core/application/commands/equip-item/equip-item.command.ts
+++ src/heroes/core/application/commands/equip-item/equip-item.command.ts
@@ -1,6 +1,12 @@
 import { ICommand } from '@nestjs/cqrs';
+import { Result } from 'neverthrow';
+import { UnknownApplicationError } from '../../../../../common/core/domain/base.error';
 import { Item } from '../../../../../items/core/domain/item.entity';
 import { Hero } from '../../../domain/hero.entity';
+import {
+  HeroDoesNotOwnItem,
+  HeroNotFoundError,
+} from '../../../domain/hero.error';
 
 export class EquipItemCommand implements ICommand {
   constructor(
@@ -10,3 +16,8 @@ export class EquipItemCommand implements ICommand {
     },
   ) {}
 }
+
+export type EquipItemCommandResult = Result<
+  void,
+  HeroNotFoundError | HeroDoesNotOwnItem | UnknownApplicationError
+>;
