d2c16b95dc77b74284b6ac0acdd85ffdc12d9a15
Pierre Chabardes <pierre.chabardes@libeo.io>
2022-05-26 00:33:47 +0200

    feature(saga): create publishEvent rxjs operator

--- src/combat/core/application/combat.application.ts
+++ src/combat/core/application/combat.application.ts
@@ -1,4 +1,5 @@
 import { QueryHandlerType, CommandHandlerType } from '@nestjs/cqrs';
+import { PublishEventCommandHandler } from '../../../common/core/commands/publish-event.command-handler';
 import { AttackCommandHandler } from './commands/attack/attack.command-handler';
 import { RewardHeroCommandHandler } from './commands/reward-hero/reward-hero.command-handler';
 import { CombatSagas } from './sagas/combat.saga';
@@ -7,6 +8,7 @@ const CombatQueryHandler: QueryHandlerType[] = [];
 const CombatCommandHandler: CommandHandlerType[] = [
   AttackCommandHandler,
   RewardHeroCommandHandler,
+  PublishEventCommandHandler,
 ];
 
 export const CombatApplications = [
